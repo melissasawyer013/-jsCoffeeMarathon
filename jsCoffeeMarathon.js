d0cc550d1fed511955176f2fae9fcd4cc8d7fbf9
Melissa Sawyer <melissasawyer013@gmail.com>
2020-03-03 07:29:32 -0500

    first commit

---
+++ jsCoffeeMarathon.js
@@ -0,0 +1,29 @@
+console.log("Coffee, please!");
+let coffeeType = "Medium Roast";
+console.log(coffeeType);
+var Latte = 2;
+var Mocha = 3;
+// console.log(Latte);
+// console.log(Mocha);
+// console.log(Latte+Mocha);
+// console.log(Latte > Mocha);
+// console.log(Latte<Mocha);
+let sugarFree;
+// window.alert(sugarFree);
+let customer1 = "Kamala Khan"; drinkChoice = "Chai", quantity = 4, foodAllergy = false;
+// console.log(customer1 +" "+ drinkChoice +" "+ quantity +" "+ foodAllergy);
+// console.log(`${customer1} ${drinkChoice} ${quantity} ${foodAllergy}`);
+let Chai = 2.50
+// console.log(Chai);
+// console.log (Chai*quantity);
+Chai = 2.5;
+// console.log(Chai);
+// let totalA = drinkChoice + " " + quantity + " " + "$" + quantity * Chai;
+let totalA =  "Your Final Order is" +" " + quantity + " " + drinkChoice + " " + "for a total of $" + quantity * Chai + ".";
+let total =(`${drinkChoice} ${quantity} $ ${quantity * Chai}`);
+console.log("+++++++++++++++++++++++++++++++++++++++++++++++++");
+console.log(totalA);
+console.log("+++++++++++++++++++++++++++++++++++++++++++++++++");
+console.log("Nice choice of " + drinkChoice + ", but we recommend a Sprite.")
+// console.log(total);
+// window.alert("Your Final Order:" total);
